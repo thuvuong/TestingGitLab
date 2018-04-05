a18e5c2fbf6afd1f2ee6b7411b5ec3eb82487939
brookn3 <brooknegussie2014@gmail.com>
2018-04-05 15:29:01 -0700

    third commit

--- app/src/main/java/edu/tacoma/uw/css/thuv/testinggitlab/TestingActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/thuv/testinggitlab/TestingActivity.java
@@ -14,6 +14,7 @@ public class TestingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_testing);
 
         Log.e(TAG, "onCreate");
+        Log.w(TAG, "onCreate");
         
     }
 }
