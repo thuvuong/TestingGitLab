e9188b10fe32542e33aa3d2d3005558d9bdd972c
thuvuong <thuv@uw.edu>
2018-04-05 14:20:25 -0700

    Initial commit

---
+++ app/src/main/java/edu/tacoma/uw/css/thuv/testinggitlab/TestingActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.thuv.testinggitlab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
