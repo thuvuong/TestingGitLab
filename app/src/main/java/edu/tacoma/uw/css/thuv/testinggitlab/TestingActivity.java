1c6f673f2d3a65b0a1615a4a363ef76d27fa9edc
brookn3 <brooknegussie2014@gmail.com>
2018-04-05 15:26:47 -0700

    second commit

--- app/src/main/java/edu/tacoma/uw/css/thuv/testinggitlab/TestingActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/thuv/testinggitlab/TestingActivity.java
@@ -2,13 +2,18 @@ package edu.tacoma.uw.css.thuv.testinggitlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TestingActivity extends AppCompatActivity {
+
+    private static final String TAG = "TestingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+
+        Log.e(TAG, "onCreate");
         
     }
 }
